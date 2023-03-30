5037e6e4f3741d7b79e1ba9a9310b08a335fce02
Hossain Saddam <hossaincrr7@gmail.com>
2023-03-31 00:14:44 +0600

    push from mac

--- components/Home/SlideOne.vue
+++ components/Home/SlideOne.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="o-slider-one">
     <div class="o-slider-one__container">
-      <h2 class="o-slider-one__title">Explain about the exclusive crypto club</h2>
+      <h2 class="o-slider-one__title" ref="s1title" ><span ref="s1titlet">Explain about the</span> <span ref="s1titlet1">exclusive crypto club</span> </h2>
       <div class="o-slider-one__image">
         <img src="~/assets/images/Main-video.jpg" alt="" />
       </div>
@@ -14,9 +14,60 @@
 export default {
   name: "HomeSlideOne",
 
-  mounted() {},
+  mounted() {
+    this.animations()
+  },
 
-  methods: {},
+  methods: {
+    animations() {
+      const gsap = this.$gsap,
+      Observer = this.$Observer;
+
+      gsap.registerPlugin(Observer);
+
+
+      const title = this.$refs.s1title
+      const split = new SplitText(this.$refs.s1titlet);
+      const split1 = new SplitText(this.$refs.s1titlet1);
+      console.log(split);
+
+      let scrollDelta
+      const tl = gsap.timeline()
+      tl.pause()
+      tl.from([split.chars, split1.chars], {
+            y: '50%',
+            autoAlpha: 0,
+            ease: "power4.out",
+            duration: 1.5,
+            immediateRender: true,
+            ease: "cubic",
+            stagger: 0.05
+          });
+
+      const obsc1 = Observer.create({
+        target: ".p-index",
+        type: "touch,scroll,pointer, wheel",
+        axis: "x",
+        onChangeX: (self) => {
+          scrollDelta += -self.deltaX;
+          if (scrollDelta < 0) {
+            scrollDelta = 0;
+          }
+          if (scrollDelta > innerWidth * 4) {
+            scrollDelta = innerWidth * 4;
+          }
+
+        const titleRect = title.getBoundingClientRect()
+
+        if(titleRect.left <= innerWidth/2) {
+          tl.play()
+        }
+          
+        },
+      });
+
+    }
+  },
 
   computed: {},
 
